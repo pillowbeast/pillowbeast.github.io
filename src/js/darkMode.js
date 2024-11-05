26df61d9708e53c49249ee46ef1e9c49c2db8d25
Yannik Werner <yannik.werner@yahoo.de>
2024-11-05 10:24:30 +0100

    toggle darkmode

--- src/js/darkMode.js
+++ src/js/darkMode.js
@@ -1,6 +1,12 @@
-export function toggleDarkMode() {
-    const toggleButton = document.querySelector('.dark-mode-toggle');
+export function attachDarkModeToggle(toggleButton, toggleImage) {
+
     toggleButton.addEventListener('click', () => {
-        document.body.classList.toggle('dark-mode');
+        document.body.classList.toggle('dark');
+        const isDarkMode = document.body.classList.contains('dark');
+
+        toggleImage.src = isDarkMode ? "/src/assets/icons/moon-solid.svg" : "/src/assets/icons/sun-solid.svg";
+
+        localStorage.setItem('darkMode', isDarkMode ? 'enabled' : 'disabled');
     });
+
 }
--- src/js/main.js
+++ src/js/main.js
@@ -1,19 +1,45 @@
 import { toggleMenu } from './menu.js';
-import { toggleDarkMode } from './darkMode.js';
+import { attachDarkModeToggle } from './darkMode.js';
 
-// Function to load HTML templates
-function loadTemplate(id, file, callback) {
-    fetch(file)
+function loadTemplate(id, file) {
+    return fetch(file)
         .then(response => response.text())
         .then(data => {
-        document.getElementById(id).innerHTML = data;
-        if (callback) callback();
+            document.getElementById(id).innerHTML = data;
         });
-    }
+}
+
+
+function loadTemplates() {
+    return Promise.all([
+        loadTemplate('header', '../src/components/header.html'),
+        loadTemplate('footer', '../src/components/footer.html')
+    ]);
+}
+
+
 
 document.addEventListener('DOMContentLoaded', () => {
-    loadTemplate('header', '../src/components/header.html', toggleMenu)
-    loadTemplate('footer', '../src/components/footer.html');
+    if (localStorage.getItem('darkMode') === 'enabled') {
+        document.body.classList.add('dark');
+    }
 
-    // toggleDarkMode();
+    loadTemplates().then(() => {
+        attachToggles();
+    });
 });
+
+function attachToggles() {
+    const toggleButton = document.querySelector('.dark-mode-toggle');
+    const toggleImage = document.getElementById('dark-mode-toggle-img');
+
+    if (localStorage.getItem('darkMode') === 'enabled') {
+        document.body.classList.add('dark');
+        toggleImage.src = "/src/assets/icons/moon-solid.svg";
+    } else {
+        toggleImage.src = "/src/assets/icons/sun-solid.svg";
+    }
+
+    toggleMenu();
+    attachDarkModeToggle(toggleButton, toggleImage);
+}
