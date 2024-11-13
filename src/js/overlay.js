e1b01c8bb6fa9cdfce761933c4c60953f97bfac0
Yannik Werner <yannik.werner@yahoo.de>
2024-11-13 13:25:50 +0100

    About with Timeline: functional

--- src/js/overlay.js
+++ src/js/overlay.js
@@ -3,12 +3,15 @@ export function handleOverlay() {
     function makeFullscreen() {
         const main = document.querySelector('main');
         main.querySelectorAll('img').forEach(image => {
-            image.addEventListener('click', () => {
-                const overlay = document.getElementById('image-overlay');
-                const overlayImage = document.getElementById('overlay-image');
-                overlayImage.src = image.src;
-                overlay.style.display = 'flex';
-            });
+            if (image.classList.contains("no-overlay")){
+            } else {
+                image.addEventListener('click', () => {
+                    const overlay = document.getElementById('image-overlay');
+                    const overlayImage = document.getElementById('overlay-image');
+                    overlayImage.src = image.src;
+                    overlay.style.display = 'flex';
+                });
+            }
         });
     }
 
